aac1354ee2c87b0f9aa55da9ef00b7fe34678931
Charumathi Badrinath <charumathi_badrinath@ryecountryday.org>
2017-12-09 22:43:16 -0500

    added features
    refactoring/replay options/fixed win logic

--- Board.java
+++ Board.java
@@ -3,13 +3,15 @@ import java.awt.*;
 import java.util.Scanner;
 public class Board extends JPanel
 {
-    TicTacToe t = new TicTacToe();
     int row;
     int column;
     Player[][] players = new Player[3][3];  
-    String letter = "";
-    int counter = 0;
     Scanner in = new Scanner(System.in);
+    boolean isX = false ; 
+    int EMPTY = -100 ;
+    String X = "X";
+    String O = "O" ;
+    boolean alreadyPrinted = false;
     //create a 3X3 grid
     public void paint(Graphics g){
         super.paintComponent(g);
@@ -29,12 +31,7 @@ public class Board extends JPanel
         g.setColor(Color.cyan);
         Font font = new Font("Apple Chancery", Font.BOLD, 30);
         g.setFont(font);
-        if(hasWon("O")){
-            g.drawString("GAME OVER: O WINS", 85, 250);
-        } 
-        if(hasWon("X")){
-            g.drawString("GAME OVER: X WINS", 85, 250);
-        }
+        g.drawString("GAME OVER: " + (hasWon(X) ? X : O) + " WINS", 85, 250);
     }
 
     private void drawPlayersInGrid(Graphics g) { 
@@ -55,54 +52,77 @@ public class Board extends JPanel
         g.fillRect(0, (500/3)*2, 500, 10);  
     }
 
+    
+    private void resetGame() { 
+        instantiatePlayers();
+        in.reset();
+        isX = false ; 
+    }
+    
+    
     public void input(){
         if (checkForWin()) { 
-            in.close();
-        }else { 
-            counter++;
             System.out.println("\u000c");
+            System.out.println("THANKS FOR PLAYING. HOPE YOU ENJOYED IT ....\n\n");
+            System.out.print("DO YOU WANT TO PLAY AGAIN (Y/N)?   ");
+            String option = in.next();
+            if (option.startsWith("Y") || option.startsWith("y")) { 
+                resetGame();
+            }else { 
+                in.close();
+                System.exit(0);
+            }
+        }else { 
+            System.out.println("\u000c");
+            String xo = isX ? X : O ; 
+            System.out.println("Player " + xo + " please choose options below : " +"\n" + "\n");
             System.out.println("ENTER A ROW NUMBER BETWEEN 1 AND 3");
             row = in.nextInt();
             System.out.println("ENTER A COLUMN NUMBER BETWEEN 1 AND 3");
             column = in.nextInt();
-            if(row < 1 || column < 1 || row > 3 || column > 3){
-                counter --;
-            }
 
-            if(players[row-1][column-1].row == -100){
-                if(counter%2 == 0){
-                    players[row-1][column-1] = new Player("X", row, column, Color.red);
-                } else if(counter%2 == 1){
-                    players[row-1][column-1] = new Player("O", row, column, Color.green);
+            if ( inputInvalid(row) || inputInvalid(column)) { 
+                System.out.println("PLEASE CHOOSE VALID VALUES ... ");
+                String enter = in.next();
+            } else {
+
+                if(players[row-1][column-1].row == EMPTY){
+                    players[row-1][column-1] = new Player(xo, row, column, isX ? Color.red:Color.green);
+                    isX = !isX ;
+                }else{
+                    System.out.println("SPOT ALREADY TAKEN... PLEASE TRY AGAIN");
+                    String enter = in.next();
                 }
-            }else{
-                counter --;
             }
         }
 
+    }
+
+    private boolean inputInvalid(int i) { 
+        return ( i < 1 || i > 3)  ;
     }
 
     public void instantiatePlayers(){
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 3; j++){
-                players[i][j] = new Player("", -100, -100, Color.white);
+                players[i][j] = new Player("", EMPTY, EMPTY, Color.white);
             }
         }
     }
 
     public boolean checkForWin() { 
-        return hasWon("X") || hasWon("O");
+        return hasWon(X) || hasWon(O);
     }
 
     private boolean hasWon(String xo) { 
         return rowColumnCheck(xo) || diagonalCheck(xo);
     }
 
-    private boolean rowCheck( String xo) { 
-
-        boolean result = true ; 
-        for ( int row = 0 ; row < 3 ; row ++ ) {
-            for ( int col = 0; col < 3 ; col ++ ) { 
+    private boolean rowCheck(String xo) { 
+        boolean result = false ; 
+        for (int row = 0 ; row < 3 ; row ++ ) {
+            result = true ; 
+            for (int col = 0; col < 3 ; col ++ ) { 
                 result = result && players[row][col].letter.equals(xo) ;
             }
             if (result) 
@@ -112,8 +132,9 @@ public class Board extends JPanel
     }
 
     private boolean columnCheck( String xo) { 
-        boolean result = true ; 
+        boolean result = false; 
         for ( int col = 0 ; col < 3 ; col ++ ) {
+            result = true ; 
             for ( int row = 0; row < 3 ; row ++ ) { 
                 result = result && players[row][col].letter.equals(xo) ;
             }
@@ -124,7 +145,7 @@ public class Board extends JPanel
     }
 
     private boolean rowColumnCheck(String xo) { 
-        boolean result ; 
+        boolean result; 
         result = rowCheck(xo) || columnCheck(xo);
         return result ; 
     }
